057a21c51273d1ae396bdcff7482ef321912b3be
Emna Ghariani <eghariani@dom.tti>
2022-07-20 18:41:24 +0100

    act 6.2.2 posts with comments

--- Activity-6.1/Act-6.1.2/src/Controller/ArticlesController.php
+++ Activity-6.1/Act-6.1.2/src/Controller/ArticlesController.php
@@ -11,18 +11,107 @@ use GuzzleHttp\Client;
 class ArticlesController extends AbstractController
 {
     /**
-     * @Route("/articles", name="app_articles")
+     * @Route("/get", name="GET")
      */
-    public function index(): Response
+    public function GetPosts(): Response
     {
-    $client = new Client(['base_uri' => 'https://jsonplaceholder.typicode.com/']);
-    $response = $client->request('GET', '/posts');
-    $body = $response->getBody();
-    $articles = json_decode($body);
+        $client = new Client(['base_uri' => 'https://jsonplaceholder.typicode.com/']);
+        $response = $client->request('GET', '/posts');
+        $body = $response->getBody();
+        $articles = json_decode($body);
 
-        return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
-                'articles' => $articles
-        ]);
+        $response = $client->request('GET', '/comments');
+        $body = $response->getBody();
+        $comments = json_decode($body);
+
+            return $this->render('articles/index.html.twig', [
+                'controller_name' => 'ArticlesController',
+                    'articles' => $articles,
+                    'commentaires'=>$comments
+            ]);
     }
+
+     /**
+     * @Route("/post", name="POST")
+     */
+    public function postPost(): Response
+    {   $client = new Client();
+        $response = $client->request('POST', 'https://jsonplaceholder.typicode.com/posts', [
+            'form_params' => [
+                'userId' => '1',
+                'id' => '200',
+                'title' => 'Test title',
+                'body' => 'test body',
+                ]
+        ]);
+        $a=$response->getBody();
+        $code=$response->getStatusCode() ;
+        $reason = $response->getReasonPhrase(); // OK
+            return $this->render('articles/message.html.twig', [
+                    'controller_name' => 'ArticlesController',
+                    'code'=>$code ,
+                    'a'=>$a,
+                    'reason'=>$reason
+            ]);
+    }   
+
+     /**
+     * @Route("/put", name="PUT")
+     */
+    public function putPost()
+    {
+        $client = new Client();
+        $response=$client->put('https://jsonplaceholder.typicode.com/posts/1', [
+            'form_params' => [
+                    'userId' => '1',
+                'id' => '300',
+                'title' => 'put test title',
+                'body' => 'test body',
+            ],
+            'timeout' => 5
+        ]);
+        $a=$response->getBody();
+        $code=$response->getStatusCode() ;
+        $reason = $response->getReasonPhrase(); // OK
+            return $this->render('articles/message.html.twig', [
+                    'controller_name' => 'ArticlesController',
+                    'code'=>$code ,
+                    'a'=>$a,
+                    'reason'=>$reason
+            ]);
+    }
+
+     /**
+     * @Route("/delete", name="DELETE")
+     */
+    public function deletePost()
+    {
+        $client = new Client();
+        $response =$client->delete('https://jsonplaceholder.typicode.com/posts/1');
+        $code=$response->getStatusCode() ;
+        $reason = $response->getReasonPhrase(); // OK
+            return $this->render('articles/message.html.twig', [
+                    'controller_name' => 'ArticlesController',
+                    'code'=>$code ,
+                    'reason'=>$reason,
+                    'a'=>''
+            ]);
+    }
+    //         /**
+    //  * @Route("/post/{id}", name="getId")
+    //  */
+    // public function GetPostById($id): Response
+    // {
+    // $client = new Client(['base_uri' => 'https://jsonplaceholder.typicode.com/']);
+    // $response = $client->request('GET', '/posts/'.$id);
+    // $body = $response->getBody();
+    // $articles = json_decode($body);
+
+    //     return $this->render('articles/index.html.twig', [
+    //         'controller_name' => 'ArticlesController',
+    //             'articles' => $articles
+    //     ]);
+    // }
 }
+
+
--- Activity-6.1/readme.txt
+++ Activity-6.1/readme.txt
@@ -1,5 +1,6 @@
 Activity 6.1 : Client REST
 
+Act6.1.1.
 How to Import a Collection into Postman:
 
 1-Go to Get started with Postman in the home page.
@@ -9,4 +10,23 @@ How to Import a Collection into Postman:
 5-Hit the open button, and your file/collection will be imported automatically.
 You can also drag the file and drop the file in Postman’s import panel, and it will be imported automatically.
 
+Act6.1.2.
+1-Clone the repository
+https://github.com/gharianiemna/Module-6-REST-APIs.git
 
+2-Move to the file of act 6.1.2 
+cd Activity-6.1.
+cd Act-6.1.2 
+
+3-Install dependencies 
+composer install
+ 
+
+4- Run the server
+php bin/console server:run
+
+5- Follow the routes:
+- GET METHOD: "/get"
+- POST METHOD: "/post"
+- PUT METHOD: "/put"
+- DELETE METHOD: "/delete"
